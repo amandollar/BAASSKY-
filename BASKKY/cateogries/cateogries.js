e902c351afb13bdc6ff530f5fa5f22e79a45bb8a
Rishu Kumar <rishukumar9233@gmail.com>
2024-07-08 10:48:35 +0530

    created seprate pages for each of the cateogries.

---
+++ BASKKY/cateogries/cateogries.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.createCard = function(previewImage, heading, previewPath) {
+        let html = `
+        <div class="template">
+            <img src="${previewImage}" alt="Background img">
+            <p>${heading}</p>
+            <div class="btn">
+                <a href="${previewPath}" target="_blank">Preview</a>
+                <a href="">Get code</a>
+            </div>
+        </div>`;
+        document.querySelector(".content").innerHTML += html;
+    };
+
+    // Add initial cards if needed
+    // createCard("/Components/Buttons/assets/blob-btn.png", "Blob Button", "/Components/Buttons/Blob Button/index.html");
+});
