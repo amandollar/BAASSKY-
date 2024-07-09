8c6aa5605d55bf4340ce0878675cd3297fe4fae4
Rishu Kumar <rishukumar9233@gmail.com>
2024-07-09 11:29:53 +0530

    redesigned nav bar

--- BASKKY/home.js
+++ BASKKY/home.js
@@ -10,6 +10,8 @@ toggleBtn.addEventListener("click", () => {
   toggleBtnIcon.classList.add(isOpen ? "fa-xmark" : "fa-bars");
 });
 
+
+
 document.addEventListener('scroll', () => {
   const header = document.querySelector("header");
   const logoLink = document.querySelector(".logo a");
